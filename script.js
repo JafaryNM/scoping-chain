e73db7c004cdba94c50467d13a766cdfcda19509
JAFARY <jaffarymdegela@gmail.com>
2025-08-12 17:53:29 +0300

    variable-hosting

--- script.js
+++ script.js
@@ -1,33 +1,40 @@
 'use strict';
 
-function calcAge(birthYear) {
-  const age = 2037 - birthYear;
-  function printAge() {
-    let output = `${firstName} have ${age}, born in ${birthYear}`;
-    console.log(output);
-    if (birthYear >= 1982 && birthYear < 1996) {
-      // Var not apply block scope
-      var millenium = true;
+// function calcAge(birthYear) {
+//   const age = 2037 - birthYear;
+//   function printAge() {
+//     let output = `${firstName} have ${age}, born in ${birthYear}`;
+//     console.log(output);
+//     if (birthYear >= 1982 && birthYear < 1996) {
+//       // Var not apply block scope
+//       var millenium = true;
 
-      //  Reassigning outer scope
-      output = 'output';
-      const firstName = 'Mosses';
-      const str = `Ohh ${firstName} you are millenium`;
-      console.log(str);
-    }
-    console.log(millenium);
-    function add(a, b) {
-      return a + b;
-    }
-    add(1, 2);
-    output = 'Output';
-    // console.log(output);
-  }
-  printAge();
-}
-// console.log(add(1, 2));
-// Scopping chain problem
-// return age;
+//       //  Reassigning outer scope
+//       output = 'output';
+//       const firstName = 'Mosses';
+//       const str = `Ohh ${firstName} you are millenium`;
+//       console.log(str);
+//     }
+//     console.log(millenium);
+//     function add(a, b) {
+//       return a + b;
+//     }
+//     add(1, 2);
+//     output = 'Output';
+//     // console.log(output);
+//   }
+//   printAge();
+// }
+// // console.log(add(1, 2));
+// // Scopping chain problem
+// // return age;
 
-const firstName = 'Jafary';
-calcAge(1991);
+// const firstName = 'Jafary';
+// calcAge(1991);
+
+console.log(me);
+console.log(job);
+console.log(year);
+var me = 'jafary';
+let job = 'Javascript Developer';
+const year = 1995;
