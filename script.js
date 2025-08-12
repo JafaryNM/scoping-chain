a7cfbf50f776e1f3b4b51cc75d9c99fc81423c8c
JAFARY <jaffarymdegela@gmail.com>
2025-08-12 17:19:31 +0300

    scoping-chain-actions

--- script.js
+++ script.js
@@ -3,12 +3,31 @@
 function calcAge(birthYear) {
   const age = 2037 - birthYear;
   function printAge() {
-    const output = `You have ${age}, born in ${birthYear}`;
+    let output = `${firstName} have ${age}, born in ${birthYear}`;
     console.log(output);
+    if (birthYear >= 1982 && birthYear < 1996) {
+      // Var not apply block scope
+      var millenium = true;
+
+      //  Reassigning outer scope
+      output = 'output';
+      const firstName = 'Mosses';
+      const str = `Ohh ${firstName} you are millenium`;
+      console.log(str);
+    }
+    console.log(millenium);
+    function add(a, b) {
+      return a + b;
+    }
+    add(1, 2);
+    output = 'Output';
+    // console.log(output);
   }
   printAge();
 }
-return age;
+// console.log(add(1, 2));
+// Scopping chain problem
+// return age;
 
 const firstName = 'Jafary';
 calcAge(1991);
