bd61e9167a20dd8a740969f81161be45f1452695
JAFARY <jaffarymdegela@gmail.com>
2025-08-12 15:39:04 +0300

    working-scoping-chain

---
+++ script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+  function printAge() {
+    const output = `You have ${age}, born in ${birthYear}`;
+    console.log(output);
+  }
+  printAge();
+}
+return age;
+
+const firstName = 'Jafary';
+calcAge(1991);
